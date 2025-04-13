e041d64e3392cb81e59f3ff360cb3db0c2f85fbd
Justin Portell <72322829+just-port@users.noreply.github.com>
2025-04-13 13:08:38 -0500

    initial chat code

---
+++ chat/api/services/cap/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+
+	"github.com/just-port/myusdl/chat/foundation/logger"
+)
+
+func main() {
+	var log *logger.Logger
+
+	traceIDFn := func(ctx context.Context) string {
+		return "" // TODO: Get traceIDs
+	}
+
+	log = logger.New(os.Stdout, logger.LevelInfo, "CAP", traceIDFn)
+
+	// ----------------------------------------------------------------------------------
+
+	ctx := context.Background()
+
+	if err := run(ctx, log); err != nil {
+		log.Error(ctx, "startup", "err", err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context, log *logger.Logger) error {
+	// -------------------------------------------------------------------------
+	// GOMAXPROCS
+
+	log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	// -------------------------------------------------------------------------
+
+	log.Info(ctx, "startup", "status", "started")
+	defer log.Info(ctx, "startup", "status", "shutting down")
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	<-shutdown
+
+	return nil
+}
--- makefile
+++ makefile
@@ -3,6 +3,12 @@ SHELL_PATH = /bin/ash
 SHELL = $(if $(wildcard $(SHELL_PATH)),/bin/ash,/bin/bash)
 
 # ==============================================================================
+# Chat
+
+chat-run:
+	go run ./chat/api/services/cap/main.go | go run ./chat/api/tooling/logfmt/main.go
+
+# ==============================================================================
 # Modules Support
 
 tidy:
